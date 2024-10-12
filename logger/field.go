6795f19a97cf36aba0c7ba4d36157990967ce9f9
David Wartell <d@wartell.net>
2024-10-11 20:14:16 -0400

    fix the issue with uber zap errorVerbose and its horrible stack trace strings

--- logger/field.go
+++ logger/field.go
@@ -316,7 +316,9 @@ func Inline(val zapcore.ObjectMarshaler) Field { return Field(zap.Inline(val)) }
 func Any(key string, value interface{}) Field { return Field(zap.Any(key, value)) }
 
 // Error is shorthand for the common idiom NamedError("error", err).
-func Error(err error) Field { return Field(zap.Error(err)) }
+func Error(err error) Field {
+	return Field(zap.String("error", err.Error()))
+}
 
 // NamedError constructs a field that lazily stores err.Error() under the
 // provided key. Errors which also implement fmt.Formatter (like those produced
@@ -336,6 +338,9 @@ func fieldsContainContextCancelled(fields ...Field) bool {
 			if fieldIsError && fieldErr != nil && strings.Contains(fieldErr.Error(), "context canceled") {
 				return true
 			}
+		}
+		if fields[i].Type == zapcore.StringType && strings.Contains(fields[i].String, "context canceled") {
+			return true
 		}
 	}
 	return false
